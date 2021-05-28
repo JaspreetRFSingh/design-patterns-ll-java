dede7a834c35d2fd486985340c9da27d45dd94a6
Jaspreet Singh <jaspreetsinghtuli@gmail.com>
2021-05-28 09:03:41 +0530

    added duck interface and subclass

---
+++ src/structural/adapter/MallardDuck.java
@@ -0,0 +1,15 @@
+package structural.adapter;
+
+public class MallardDuck implements Duck {
+
+	@Override
+	public void quack() {
+		System.out.println("Quack Quack!");
+	}
+
+	@Override
+	public void fly() {
+		System.out.println("Duck is flying");
+	}
+
+}
