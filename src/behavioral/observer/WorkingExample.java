6aa17500ab57b429cc695267021bf0cfad5cf32c
Jaspreet Singh <jaspreetsinghtuli@gmail.com>
2021-05-30 08:29:43 +0530

    tested the removal of an observer

--- src/behavioral/observer/WorkingExample.java
+++ src/behavioral/observer/WorkingExample.java
@@ -5,8 +5,10 @@ public class WorkingExample {
 		WeatherStation weatherStation = new WeatherStation();
 		new Logger(weatherStation);
 		new Alert(weatherStation);
-		new UserInterface(weatherStation);
+		Observer userInterface = new UserInterface(weatherStation);
 		weatherStation.setMeasurements(23.56, 623.48, 85.22);
 		weatherStation.setMeasurements(56.96, 423.91, 43.77);
+		weatherStation.removeObserver(userInterface);
+		weatherStation.setMeasurements(26.96, 23.91, 43.75);
 	}
 }
