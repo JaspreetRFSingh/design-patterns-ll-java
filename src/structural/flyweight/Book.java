eed999a839bf7b08a26e7c5c72a6c5cb43016d9f
Jaspreet Singh <jaspreetsinghtuli@gmail.com>
2024-02-22 22:22:26 +0530

    flyweight design pattern

---
+++ src/structural/flyweight/Book.java
@@ -0,0 +1,13 @@
+package structural.flyweight;
+
+public class Book {
+    private final String name;
+    private final double price;
+    private final BookType type;
+
+    public Book(String name, double price, BookType type) {
+        this.name = name;
+        this.price = price;
+        this.type = type;
+    }
+}
